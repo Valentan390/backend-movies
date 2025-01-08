aa2676efc6371c3df35768cceface22c09baffdc
Valentan390 <valentynhavrylenko8886@gmail.com>
2025-01-08 16:44:46 +0200

    Update jwtToken.ts

--- src/utils/jwtToken.ts
+++ src/utils/jwtToken.ts
@@ -1,26 +1,26 @@
-import jwt from 'jsonwebtoken';
-import { env } from './env';
+// import jwt from 'jsonwebtoken';
+// import { env } from './env';
 
-const jwtSecret = env('JWT_SECRET');
+// const jwtSecret = env('JWT_SECRET');
 
-export interface CreateTokenParams {
-  email: string;
-}
+// export interface CreateTokenParams {
+//   email: string;
+// }
 
-export const createToken = (payload: CreateTokenParams) =>
-  jwt.sign(payload, jwtSecret, { expiresIn: '24h' });
+// export const createToken = (payload: CreateTokenParams) =>
+//   jwt.sign(payload, jwtSecret, { expiresIn: '24h' });
 
-export const verifyToken = (token: string) => {
-  try {
-    const payload = jwt.verify(token, jwtSecret);
-    return {
-      data: payload,
-      error: null,
-    };
-  } catch (error) {
-    return {
-      error,
-      data: null,
-    };
-  }
-};
+// export const verifyToken = (token: string) => {
+//   try {
+//     const payload = jwt.verify(token, jwtSecret);
+//     return {
+//       data: payload,
+//       error: null,
+//     };
+//   } catch (error) {
+//     return {
+//       error,
+//       data: null,
+//     };
+//   }
+// };
